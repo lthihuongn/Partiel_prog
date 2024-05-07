9e8717c542ee77fef1034809417c12612d8ab6b9
lthihuongn <lilou33127v@gmail.com>
2024-05-07 11:27:26 +0200

    commit

--- ex1.c
+++ ex1.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    int var1 = 6;
-    int var2 = 4;
+    int var1 =6;
+    int var2 = 5;
+    int *ptr1;
 
 
 
-    var1, var2 = var2, var1;
+    // Allocation dynamique de mémoire pour 5 entiers
+    ptr1 = (int *)malloc(5 * sizeof(int));
 
-    printf("%d\n", var1);
-    printf("%d\n", var2);
+
+    // Utilisation de la mémoire allouée
+    ptr1[0] = var1;
+    var1 = var2;
+    var2 = ptr1[0];
+    printf("%d, %d", var1, var2);
+
+
+
+    
+
+    // Libération de la mémoire allouée
+    free(ptr1);
 
 
     return 0;
